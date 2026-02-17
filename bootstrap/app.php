45572196e90c89a01b157edb9e94d55acb23551c
hadis <hadis.pcprogram@gmail.com>
2026-02-17 16:53:57 +0330

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,36 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Project uses Laravel 12
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->group('api', [
+            'auth.jwt' => \App\Http\Middleware\AuthWithJwt::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->withSchedule(function (Illuminate\Console\Scheduling\Schedule $schedule) {
+        $schedule->command('test:schedule')->everyMinute();
+        $schedule->command('cache:clear-expired')->everyFiveMinutes();
+
+        $schedule->command('queue:work --queue=fastJob --sleep=3 --tries=3 --timeout=120')
+            ->withoutOverlapping()
+            ->runInBackground()
+            ->everyMinute();
+
+        $schedule->command('queue:work --queue=queueJob --sleep=3 --tries=3 --timeout=120')
+            ->withoutOverlapping()
+            ->runInBackground()
+            ->everyTwoMinutes();
+
+        $schedule->command('cache:backup-delete-flights')->dailyAt('00:00');
+    })
+    ->create();
